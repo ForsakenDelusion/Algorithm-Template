2050af04b0d604858718a18aa24ecbea0d14c8f7
ForsakenDelusion <wangcy0205@gmail.com>
2026-01-02 21:39:48 +0800

    修复高精度减法

--- 高精度/高精度减法.cpp
+++ 高精度/高精度减法.cpp
@@ -34,10 +34,16 @@ vector<int>  BigNumSub(const string &a, const string& b){
         int c;
         c = va[i] - cn;
         if(i < b.size()) c -= vb[i];
-        cn = 0;
-        if(va[i] < vb[i]) cn = 1;
-        res.push_back((c+10)%10);
+
+        if(c < 0) {
+            c += 10;
+            cn = 1;
+        } else {
+            cn = 0;
+        }
+        res.push_back(c);
     }
+    while (res.size() > 1 && res.back() == 0) res.pop_back();
     return res;
 }
 
