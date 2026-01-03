b9a4ef14b50ce9f3d8263c77a2daf3c2ce3f1513
ForsakenDelusion <wangcy0205@gmail.com>
2026-01-03 10:34:15 +0800

    update

--- 排序算法/topk-快排.cpp
+++ 排序算法/topk-快排.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 const int N = 1e5 + 10;
 
-void quick_sort(int* A, int l, int r) {
-  if (l == r) return;  // 递归终止
-
+void quick_sort(int A[], int l, int r, int target) {
+  if (l == r) {
+      cout << A[l];
+      return;  // 递归终止
+}
   // 两个边界要向外面扩大一格，因为我们后面做的是do while
   int i = l - 1;
   int j = r + 1;
@@ -20,23 +22,23 @@ void quick_sort(int* A, int l, int r) {
     while (A[j] > pivot);
     if (i < j) swap(A[i], A[j]);
   }
-  quick_sort(A, l, j);
-  quick_sort(A, j + 1, r);
+  if(target <= j) return quick_sort(A,l,j,target); // 剪枝操作，因为排序完的序列肯定是递增的，所以我们只需要对第k的元素所在的区间用快排就行了，最后top k肯定落在那一段上
+  else return quick_sort(A,j+1,r,target);
 }
 
 int main() {
   int a[N];
   int n;
+  int target_idx;
   cin >> n;
+  cin >> target_idx;
+  target_idx -= 1; // 适应数组下标
   for (int i = 0; i < n; i++) {
     cin >> a[i];
   }
 
-  quick_sort(a, 0, n - 1);
+  quick_sort(a, 0, n - 1,target_idx );
 
-  for (int i = 0; i < n; i++) {
-    cout << a[i] << ' ';
-  }
 
   return 0;
 }
